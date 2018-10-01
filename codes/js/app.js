6e9e7ab84bdc3503b8de7fc93f9a455252d0cdf0
Ben <WBHankins93@Bens-MacBook-Air.local>
2018-10-01 12:55:12 -0500

    First commit, created array for logos and names

---
+++ codes/js/app.js
@@ -0,0 +1,69 @@
+var userInput = "";
+var score = 0;
+var index = 0;
+
+
+var logos = [
+  {
+    "brand": "Pepsi",
+    "logo": "logos/pepsi.png"
+  }, {
+    "brand": "Reddit",
+    "logo": "logos/reddit.png"
+  }, {
+    "brand": "Lexus",
+    "logo": "logos/lexus.png"
+  }, {
+    "brand": "Versace",
+    "logo": "logos/versace.jpg"
+  }, {
+    "brand": "Lays",
+    "logo": "logos/lays.jpg"
+  }, {
+    "brand": "Firefox",
+    "logo": "logos/firefox.jpg"
+  }, {
+    "brand": "Lambo",
+    "logo": "logos/lambo.jpg"
+  }, {
+    "brand": "Nike",
+    "logo": "logos/nike.jpg"
+  }, {
+    "brand": "Starbucks",
+    "logo": "logos/starbucks.jpg"
+  }, {
+    "brand": "Blackberry",
+    "logo": "logos/blackberry.jpg"
+  }, {
+    "brand": "Cartoon Network",
+    "logo": "logos/cartoon-network.jpg"
+  }, {
+    "brand": "Jagermeister",
+    "logo": "logos/jagermeister.jpg"
+  }, {
+    "brand": "Bridgestone",
+    "logo": "logos/bridgestone.jpg"
+  }, {
+    "brand": "BMW",
+    "logo": "logos/bmw.jpg"
+  }, {
+    "brand": "Best Buy",
+    "logo": "logos/bestbuy.gif"
+  }, {
+    "brand": "Uber",
+    "logo": "logos/uber.jpg"
+  }, {
+    "brand": "Bacardi",
+    "logo": "logos/bacardi.jpg"
+  }, {
+    "brand": "Stella Artois",
+    "logo": "logos/stella-artois.png"
+  }, {
+    "brand": "Quaker Oats",
+    "logo": "logos/quaker-oats.gif"
+  }, {
+    "brand": "Subaru",
+    "logo": "logos/subaru.jpg"
+  }
+];
+console.log(logos[index].logo);
